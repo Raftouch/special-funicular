a388f8106eb3177e6ff378af797108cf97659aa7
raftouch <ntimorshina@gmail.com>
2025-03-29 16:02:20 +0100

    add decode passw, the issue with master passw to be fixed

--- password_manager.py
+++ password_manager.py
@@ -1,6 +1,5 @@
 from cryptography.fernet import Fernet
 
-
 '''
 def write_key():
     key = Fernet.generate_key()
@@ -14,19 +13,17 @@ def load_key():
     file.close()
     return key
 
-
-master_password = input("What is the master password? ")
-key = load_key() + master_password.encode()
+# master_password = input("What is the master password? ")
+# key = load_key() + master_password.encode()
+key = load_key()
 fer = Fernet(key)
-
 
 def view():
     with open('passwords.txt', "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, password = data.split("|")
-            print("User:", user, "- Password:", str(fer.decrypt(password.encode())))
-
+            print("User:", user, "| Password:", fer.decrypt(password.encode()).decode())
 
 def add():
     name = input("Account name: ")
@@ -36,7 +33,7 @@ def add():
 # r --> read
 # a --> append (add smth at the end of the file and create a new file if that file does not exist)
     with open('passwords.txt', "a") as f:
-        f.write(name + "|" + str(fer.encrypt(password.encode())) + "\n")
+        f.write(name + "|" + fer.encrypt(password.encode()).decode() + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit? ").lower()
