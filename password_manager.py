0cb52348dde280a24680dc714d51a3df5e55108b
raftouch <ntimorshina@gmail.com>
2025-03-29 15:48:04 +0100

    encrypt passwords

--- password_manager.py
+++ password_manager.py
@@ -1,11 +1,32 @@
-password = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+master_password = input("What is the master password? ")
+key = load_key() + master_password.encode()
+fer = Fernet(key)
+
 
 def view():
     with open('passwords.txt', "r") as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, passw = data.split("|")
-            print("User:", user, "- Password:", passw)
+            user, password = data.split("|")
+            print("User:", user, "- Password:", str(fer.decrypt(password.encode())))
+
 
 def add():
     name = input("Account name: ")
@@ -15,7 +36,7 @@ def add():
 # r --> read
 # a --> append (add smth at the end of the file and create a new file if that file does not exist)
     with open('passwords.txt', "a") as f:
-        f.write(name + "|" + password + "\n")
+        f.write(name + "|" + str(fer.encrypt(password.encode())) + "\n")
 
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press Q to quit? ").lower()
